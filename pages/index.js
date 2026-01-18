b9c3305c8bf9a456f587b3ae16efeb13e2fe1d9c
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2026-01-18 01:06:12 -0500

    upgrade node

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ images }) {
   return (
     <Layout
       title="Jacob Goldfarb"
-      keywords="jacob, goldfarb, portfolio, software engineer, developer"
+      keywords="jacob, goldfarb, portfolio, software engineer, developer, gptzero"
     >
       <section className="min-h-[80vh] flex items-center">
         <div className="container mx-auto px-4 md:px-8">
